f832fe8464fbb5391de57a6499c7dfac30f0c0b2
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2023-05-18 07:15:38 +0300

    MenuScene done weapon swap and some bugs

---
+++ GDProject/Assets/Scripts/RoomsEngine/MenuLoader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoader : MonoBehaviour
+{
+    public bool replanishHealth = true;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeValue(bool value)
+    {
+        replanishHealth = value;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
